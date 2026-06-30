780e882320ec64ec127bedd5bf26b553d4c488ab
niko5443 <184934855+Nika221-bit@users.noreply.github.com>
2026-06-30 22:14:46 +0400

    done

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+   
+    return (
+        <footer>
+            <p>&copy; your website Name</p>
+            
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+      
+    return(
+          <header>
+            my website
+              <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+                <hr></hr>
+              </nav>
+             
+          </header>
+    )
+}
+export default Header
